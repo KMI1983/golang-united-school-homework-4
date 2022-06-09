2c861f2f74a67bb06f5c52f8fe3dc39248e68965
KMI1983 <minskkmi@gmail.com>
2022-06-09 22:21:46 +0300

    feat string sum

--- string_sum.go
+++ string_sum.go
@@ -2,26 +2,73 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
-	// Use when the input is empty, and input is considered empty if the string contains only whitespace
-	errorEmptyInput = errors.New("input is empty")
-	// Use when the expression has number of operands not equal to two
+	errorEmptyInput     = errors.New("input is empty")
 	errorNotTwoOperands = errors.New("expecting two operands, but received more or less")
+	errorNotValidValue  = errors.New("input expression is not valid")
 )
-
-// Implement a function that computes the sum of two int numbers written as a string
-// For example, having an input string "3+5", it should return output string "8" and nil error
-// Consider cases, when operands are negative ("-3+5" or "-3-5") and when input string contains whitespace (" 3 + 5 ")
-//
-//For the cases, when the input expression is not valid(contains characters, that are not numbers, +, - or whitespace)
-// the function should return an empty string and an appropriate error from strconv package wrapped into your own error
-// with fmt.Errorf function
-//
-// Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	input = strings.ReplaceAll(input, " ", "")
+	if len(input) == 0 {
+		return "", fmt.Errorf("Empty input error: %w", errorEmptyInput)
+	}
+	match, _ := regexp.MatchString(`^\D*[0-9]+\D+[0-9]+\D*$`, input)
+	if !match {
+		return "", fmt.Errorf("Not Two Operands error: %w", errorNotTwoOperands)
+	}
+
+	match, _ = regexp.MatchString(`^([\+\-]?[0-9]+[\+\-]{1}[0-9]+)$`, input)
+	if !match {
+		return "", fmt.Errorf("Incorrect input error: %w", errorNotValidValue)
+	}
+
+	firstOperator := ""
+	secondOperator := ""
+	for i := 0; i < len(input); i++ {
+		if string(input[i]) == "+" || string(input[i]) == "-" {
+			if i == 0 {
+				firstOperator = string(input[i])
+			} else {
+				secondOperator = string(input[i])
+			}
+		}
+	}
+
+	reg := regexp.MustCompile(`[\+\-]`)
+	operands := reg.Split(input, -1)
+	var correctOperands []string
+	for i := 0; i < len(operands); i++ {
+		if operands[i] != "" {
+			correctOperands = append(correctOperands, operands[i])
+		}
+	}
+
+	firstOperand, err := strconv.Atoi(correctOperands[0])
+	if err != nil {
+		return "", fmt.Errorf("Parsing first operand error: %w", err)
+	}
+
+	secondOperand, err := strconv.Atoi(correctOperands[1])
+	if err != nil {
+		return "", fmt.Errorf("Parsing second operand error: %w", err)
+	}
+
+	if firstOperator == "-" {
+		firstOperand = firstOperand * -1
+	}
+	sum := 0
+	if secondOperator == "+" {
+		sum = firstOperand + secondOperand
+	} else {
+		sum = firstOperand - secondOperand
+	}
+
+	return strconv.Itoa(sum), nil
 }
